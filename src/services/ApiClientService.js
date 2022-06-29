6abd10903331e75dcc870bd99406d96288e929da
sebasfdz <fdz.seb@gmail.com>
2022-06-29 10:19:09 +0200

    Fix: Commented out Kanban board useEffect Api calls to fix bug.

--- src/services/ApiClientService.js
+++ src/services/ApiClientService.js
@@ -1,8 +1,20 @@
+import axios from 'axios';
+
 export const ApiClientService = {
   getKanbanBoard: async board_id => {
-    const result = await fetch(
+    const board = await axios.get(
       `https://ugmp3ddru7.execute-api.us-east-1.amazonaws.com/dev/kanban/${board_id}`,
     );
-    return result;
+    return board;
+  },
+
+  updateKanbanBoard: async (board_id, columns) => {
+    return await fetch(
+      `https://ugmp3ddru7.execute-api.us-east-1.amazonaws.com/dev/kanban/${board_id}`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(columns),
+      },
+    );
   },
 };
