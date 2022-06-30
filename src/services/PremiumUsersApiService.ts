0f51a8249d9a19355f7a29ffbc226501926e5b6d
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-06-30 12:47:15 +0200

    Services: Added premium user services

---
+++ src/services/PremiumUsersApiService.ts
@@ -0,0 +1,56 @@
+const BASE_URL = 'https://arctic-desert.herokuapp.com/premium';
+
+export type UserToAddToPremium = {
+  user_id: string;
+};
+
+export type PremiumUser = {
+  time_added: string;
+  user_id: string;
+};
+
+export const addUserToPremium = async (body: UserToAddToPremium) => {
+  try {
+    const response = await fetch(BASE_URL, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+    const parsedRepsonse: PremiumUser | { message: string } =
+      await response.json();
+    return parsedRepsonse;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAllPremiumUsers = async () => {
+  try {
+    const response = await fetch(BASE_URL);
+    const parsedRepsonse: PremiumUser[] = await response.json();
+    return parsedRepsonse;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPremiumUserByUserId = async (user_id: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${user_id}`);
+    const parsedRepsonse: PremiumUser = await response.json();
+    return parsedRepsonse;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const removePremiumUserByUserId = async (user_id: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${user_id}`, {
+      method: 'DELETE',
+    });
+    const parsedRepsonse: { message: string } = await response.json();
+    return parsedRepsonse;
+  } catch (error) {
+    console.log(error);
+  }
+};
