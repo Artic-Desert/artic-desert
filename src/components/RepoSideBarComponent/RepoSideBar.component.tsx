7c5dbc36cce5c9de41f8ee7ac7bc77bce36456de
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-07-01 20:29:33 +0200

    Fix: fixed redux problem of adding repos to the state when navigating back from workspace

--- src/components/RepoSideBarComponent/RepoSideBar.component.tsx
+++ src/components/RepoSideBarComponent/RepoSideBar.component.tsx
@@ -36,26 +36,36 @@ export const RepoSideBar: React.FC = () => {
     console.log('user before', user);
 
     user &&
-      user.repos.map(async repo => {
-        try {
-          const response = await fetch(`https://api.github.com/repos/${repo}`, {
-            headers: {
-              Authorization: `token ${process.env.REACT_APP_GHP_TOKEN}`,
-            },
-          });
-          const data: GithubRepo = await response.json();
-          console.log('repo data: ', data);
-          dispatch(addRepo(data));
-        } catch (error) {
-          console.log(
-            'Error in repo side bar fetching repos from github: ',
-            error,
-          );
-        }
-      });
+      Promise.all(
+        user.repos.map(async repo => {
+          try {
+            const response = await fetch(
+              `https://api.github.com/repos/${repo}`,
+              {
+                headers: {
+                  Authorization: `token ${process.env.REACT_APP_GHP_TOKEN}`,
+                },
+              },
+            );
+            const data: GithubRepo = await response.json();
+            console.log('repo data: ', data);
+            return data;
+            // dispatch(addRepo(data));
+          } catch (error) {
+            console.log(
+              'Error in repo side bar fetching repos from github: ',
+              error,
+            );
+          }
+        }),
+      ).then(result => dispatch(setRepos(result)));
+
+    // dispatch(setRepos(allRepos));
 
     setLoading(false);
   };
+
+  useEffect(() => console.log(repos), [repos]);
 
   useEffect(() => {
     fetchUserRepos();
