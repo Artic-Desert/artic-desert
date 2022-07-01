27923f62d60736112a86ec702f22d91922c17980
sebasfdz <fdz.seb@gmail.com>
2022-07-01 18:48:29 +0200

    Solving merge conflicts

--- src/screens/DashboardScreen/Dashboard.screen.tsx
+++ src/screens/DashboardScreen/Dashboard.screen.tsx
@@ -12,9 +12,36 @@ import { useUser } from '../../hooks/use-user';
 import { AuthService } from '../../services/AuthService';
 import ades from '../../assets/ades.svg';
 import './Dashboard.css';
+import { UserInfo } from './UserInfoComponent/UserInfo.component';
+import { useDispatch } from 'react-redux';
+import { setRepo } from '../../redux/repo/actions';
+import { setBranch } from '../../redux/branch/actions';
 
 export const Dashboard: React.FC = () => {
   const { user } = useUser();
+  const [gitHubUser, setGitHubUser] = React.useState<any>();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRepo({}));
+    dispatch(setBranch('all-branches'));
+  }, []);
+
+  useEffect(() => {
+    console.log('EL USER 8===D', user);
+
+    fetch(`https://api.github.com/users/${user.username}`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GHP_TOKEN}`,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('EL LOG 8===D', data);
+        setGitHubUser(data);
+      });
+  }, []);
 
   const navigate = useNavigate();
 
@@ -27,26 +54,10 @@ export const Dashboard: React.FC = () => {
     user && (
       <div className="dashboard-wrapper">
         <div className="dashboard-left">
-          <div className="header">
-            <div className="dashboard-user-info">
-              <div className="name-pic">
-                <img src={user.avatar_url} alt="user profile pics" />
-                <div className="name">{user.name}</div>
-              </div>
-              <div className="git-link">
-                <div className="githubName">{user.login}</div>
-                <a
-                  href={user.html_url}
-                  className="githubLink"
-                  target="_blank"
-                  rel="noreferrer">
-                  <GoMarkGithub className="git-logo" />
-                </a>
-              </div>
-              <button className="logout-button" onClick={handleLogout}>
-                Log Out
-              </button>
-            </div>
+          <UserInfo />
+        </div>
+        <div className="dashboard-middle">
+          {/* <div className="header">
             <div className="title-logo-cont">
               <img className="header-logo" src={ades} alt="" />
               <h1 className="header-title">Dashboard</h1>
@@ -54,7 +65,7 @@ export const Dashboard: React.FC = () => {
             <div className="add-new">
               <h4>Add a new repository to your Dashboard</h4>
             </div>
-          </div>
+          </div> */}
           <div className="hero">
             <div className="productivity">
               <Time />
