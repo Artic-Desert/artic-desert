1bac7d5fc0ad76cdb0f44f1d28b79832d4e13d08
Andres Rodriguez <web.arod@gmail.com>
2022-07-03 21:22:54 +0200

    solved conflicts

--- src/components/GitTimelineComponent/ModalComponent/Modal.component.tsx
+++ src/components/GitTimelineComponent/ModalComponent/Modal.component.tsx
@@ -43,7 +43,6 @@ export const Modal: React.FC<{
   const { repo } = useRepo();
   console.log('MODAL FOR REPO!!!!!!! ', repoPreview);
 
-<<<<<<< HEAD
   const [commitInfo, setCommitInfo] = useState<GithubCommit>();
 
   const fetchCommitUrl = !repoPreview
@@ -61,27 +60,6 @@ export const Modal: React.FC<{
   }, []);
 
   return repoPreview ? (
-=======
-  const [repoLang, setRepoLang] = useState<any>(null);
-
-  useEffect(() => {
-    fetch(
-      `https://api.github.com/repos/${repo?.owner.login}/${repo?.name}/languages`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GHP_TOKEN}`,
-        },
-      },
-    )
-      .then(res => res.json())
-      .then(data => {
-        setRepoLang(data);
-      });
-  }, []);
-
-  return repo ? (
->>>>>>> andres
     <>
       <Backdrop onClick={handleClose}>
         <motion.div
@@ -92,7 +70,6 @@ export const Modal: React.FC<{
           exit="exit"
           onClick={e => e.stopPropagation()}>
           <div className="preview-modal-wrapper">
-<<<<<<< HEAD
             <h1>{repoPreview.name}</h1>
             <p>{repoPreview.default_branch}</p>
             <p>{repoPreview.owner.login}</p>
@@ -100,31 +77,6 @@ export const Modal: React.FC<{
             <p>{repoPreview.watchers}</p>
             <p>{repoPreview.updated_at}</p>
             <p>{repoPreview.clone_url}</p>
-=======
-            <div className="name-visibility">
-              <h1 className="modal-repo-name">{repo.name}</h1>
-              <p className="modal-repo-visibility">{repo.visibility}</p>
-            </div>
-            <p className="modal-repo-owner">Owner: {repo.owner.login}</p>
-            <p className="modal-repo-default">
-              Default Branch: {repo.default_branch}
-            </p>
-            {/* <img src={repo.owner.avatar_url} alt="" /> */}
-            <p>{repo.watchers}</p>
-            <p>{repo.updated_at}</p>
-            <p>{repo.clone_url}</p>
-            <p>{repo.size}</p>
-            <p>{repo.stargazers_count}</p>
-            {/* {repoLang
-              ? repoLang.map((lang: any) => {
-                  return <h1 key={lang.CSS}></h1>;
-                })
-              : null} */}
-            <div>{repoLang?.CSS}</div>
-            <div>{repoLang?.HTML}</div>
-            <div>{repoLang?.JavaScript}</div>
-            <div>{repoLang?.TypeScript}</div>
->>>>>>> andres
           </div>
         </motion.div>
       </Backdrop>
