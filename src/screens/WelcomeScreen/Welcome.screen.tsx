f3e5f914a23c52ea1418a46f5d8e77bb75a6360f
Andres Rodriguez <web.arod@gmail.com>
2022-07-05 11:30:33 +0200

    testing lottie

--- src/screens/WelcomeScreen/Welcome.screen.tsx
+++ src/screens/WelcomeScreen/Welcome.screen.tsx
@@ -1,11 +1,24 @@
 // import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { AiOutlineGithub } from 'react-icons/ai';
+import lottie from 'lottie-web';
 import mask from '../../assets/mask.svg';
 import './Welcome.css';
 
 export const Welcome: React.FC = () => {
   const client_id = '71c6863d3d338f86fe07';
+
+  const container: any = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../assets/lottie/teamlottie.json'),
+    });
+  }, []);
 
   return (
     <div className="welcome-wrapper">
@@ -13,9 +26,10 @@ export const Welcome: React.FC = () => {
         <h1 className="top"> &lt;Arctic </h1>
         <h1 className="bot"> Desert/&gt; </h1>
       </div>
-      <div className="main-title">
+      {/* <div className="main-title">
         <h1>home of collaborative development.</h1>
-      </div>
+      </div> */}
+      <div className="lottie-container" ref={container}></div>
       <button className="login-button">
         <a
           className="a-tag"
@@ -24,7 +38,7 @@ export const Welcome: React.FC = () => {
         </a>
         <AiOutlineGithub fontSize="45px" />
       </button>
-      <img className="mask" src={mask} alt="" />
+      {/* <img className="mask" src={mask} alt="" /> */}
     </div>
   );
 };
