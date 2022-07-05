1abb3c156daad84b86409402cdee09ed1980a18e
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-07-05 13:25:28 +0200

    Refactor: Changed GithubApiService.js into ts

--- src/services/GithubApiService.ts
+++ src/services/GithubApiService.ts
@@ -1,9 +1,13 @@
+import { GithubUser } from '../types/Types';
+
 export const GithubApiService = {
-  getUser: async username => {
+  getUser: async (username: string): Promise<GithubUser> => {
     return fetch(`https://api.github.com/users/${username}`, {
       headers: {
         Authorization: 'token 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',
       },
-    }).then(res => res.json());
+    })
+      .then(res => res.json())
+      .catch(err => console.log(err));
   },
 };
